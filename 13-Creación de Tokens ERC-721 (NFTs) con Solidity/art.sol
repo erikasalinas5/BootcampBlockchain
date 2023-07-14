224823bbe3f630740abc024c84b41d46a6b946a5
Erika Salinas Gonzalez <108109970+erikasalinas5@users.noreply.github.com>
2023-07-13 22:45:17 -0500

    Update art.sol

--- 13-Creación de Tokens ERC-721 (NFTs) con Solidity/art.sol
+++ 13-Creación de Tokens ERC-721 (NFTs) con Solidity/art.sol
@@ -49,24 +49,37 @@ contract ArtToken is ERC721, Ownable {
     }
 
     // NFT Token Creation (Artwork)
+    // Una parte de la función se encargará de la lógica y otra del pago del usuario
     function _createArtWork(string memory _name) internal {
+        // Siempre asignar terminos divisibles por 10 
+        // o en potencias en base 10 y se convierte en uint8
         uint8 randRarity = uint8(_createRandomNum(1000));
+        // Nivel de rareza para el adn
         uint256 randDna = _createRandomNum(10**16);
+        // Se crea la obra de arte. El 1 es de un level donde el usuario puede pagar por aumentarla
         Art memory newArtWork = Art(_name, COUNTER, randDna, 1, randRarity);
+        // Se añade a la lista
         art_works.push(newArtWork);
+        // Se le asigna la obra de arte a quien la está creando
         _safeMint(msg.sender, COUNTER);
+        // Se emite el evento
         emit NewArtWork(msg.sender, COUNTER, randDna);
+        // Se cambia el counter para que siempre sea distinta
         COUNTER++;
     }
 
     // NFT Token Price Update 
+    // onlyOwner para que sea modificada solo por nosotros
+    // El onlyowner hace parte de las funciones o modificadores de la libreria ownable
     function updateFee(uint256 _fee) external onlyOwner {
         fee = _fee;
     }
 
     // Visualize the balance of the Smart Contract (ethers)
     function infoSmartContract() public view returns(address, uint256){
+        // La dirección del smart contract se toma con this y extrayendo la direccion
         address SC_address = address(this);
+        // se hace la conversión de weis a ethers
         uint256 SC_money = address(this).balance / 10**18;
         return (SC_address, SC_money);
     }
@@ -77,9 +90,14 @@ contract ArtToken is ERC721, Ownable {
     }
 
     // Obtaining a user's NFT tokens
+    // usar memory para este tipo de estructuras o para string
     function getOwnerArtWork(address _owner) public view returns (Art [] memory){
+        // el balance nos dirá cuantas obras de arte tiene
         Art [] memory result = new Art[](balanceOf(_owner));
+        // el counter va a recorrer esas obras de arte
         uint256 counter_owner = 0;
+        // Se recorren todas las obras de arte y si el owner consigue con el
+        // que estoy consultando la añade
         for (uint256 i = 0; i < art_works.length; i++){
             if (ownerOf(i) == _owner){
                 result[counter_owner] = art_works[i];
@@ -94,12 +112,16 @@ contract ArtToken is ERC721, Ownable {
     // ============================================
 
     // NFT Token Payment
+    // La funcion será pública, por eso no lleva _
     function createRandomArtWork(string memory _name) public payable {
+        // Valida que el valor que pago sea mayor o igual
+        // a la tasa necesaria a pagar
         require(msg.value >= fee);
         _createArtWork(_name);
     }
 
     // Extraction of ethers from the Smart Contract to the Owner
+    // Esta funcion sera ejecutada solo por el owner
     function withdraw() external payable onlyOwner{
         address payable _owner = payable(owner());
         _owner.transfer(address(this).balance);
@@ -107,6 +129,7 @@ contract ArtToken is ERC721, Ownable {
 
     // Level up NFT Tokens
     function levelUp(uint256 _artId) public {
+        // Validacion para que solo lo pueda hacer el owner
         require(ownerOf(_artId) == msg.sender);
         Art storage art = art_works[_artId];
         art.level++;
